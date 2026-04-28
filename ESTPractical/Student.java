f2b2bce9b42cb24d3db60fc893d49f1d8d3f5129
mehak2605 <70470410+mehak2605@users.noreply.github.com>
2026-04-28 16:24:19 +0530

    Create Student.java

---
+++ ESTPractical/Student.java
@@ -0,0 +1,56 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "course", unique = true)
+    private String course;
+
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setEmail(String email) {
+        this.email= email;
+    }
+    
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
